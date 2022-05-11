07a1e907e54b714934a4d0be0db179a66782b166
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-05-11 16:05:24 +0000

    p04

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,18 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
+hours = input("Enter hours? ")
+hr=float(hours)
+rate=input("Enter the rate per hour? ")
+rt=float(rate)
+if hr > 40:
+   pay=hr*rt
+   final_rt=(hr-40)*(rt*0.5)
+   grass_pay=pay+final_rt
+   print('Pay:',grass_pay)
+else:
+  pay=hr*rt
+  print(pay)
+print("Done !")
+
+
+
