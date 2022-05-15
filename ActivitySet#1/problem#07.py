43fde1a3b2e49ed30c805e14e3c5843c7dc46c98
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-05-15 10:44:53 +0000

    P7

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,10 @@
 # Strings
-
+"""
+ Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert the extracted value to a floating point number and print it out.
+"""
 text = "X-DSPAM-Confidence:    0.8475"
+length=len(text)
+print("Length of String is : ",length)
+print("0.8475 is @: ",text.find('0.8475'))
+slice=text[23:length]
+print(slice)
