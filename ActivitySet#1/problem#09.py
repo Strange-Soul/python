794a224bc5a8ed4d26624d1ec54b4674a49b6ec3
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-05-16 16:57:18 +0000

    Act_P2

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,8 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+fopen=open(filename)
+for line in fopen:
+  print(line.strip())
+print('Splitting of string as fallows')
+print(line)
--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,18 +1,20 @@
-
+def input_two_numbers():
+  n=input("Enter a Number to add: ")
+  return n
+  
 def add(a, b):
-    pass  # ...
-
+    return a+b
 
 def output(a, b, sum):
-    pass  # ...
-
+    print("Addition of {%d} and {%d} is :",a,b,sum)
 
 def main():
-    a, b = input_two_numbers()
+   
     sum = add(a, b)
-
     output(a, b, sum)
 
-
+a= int(input_two_numbers())
+b=int(input_two_numbers())
 if __name__ == '__main__':
-    main()
+
+  main()
