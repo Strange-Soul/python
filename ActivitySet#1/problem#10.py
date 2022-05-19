63b9d320fbb0e070980d3c1890ed9042e4d2438b
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-05-19 16:58:12 +0000

    p10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,30 @@
 # Dictionaries
 
+"""
+9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+"""        
 filename = "dataset/mbox-short.txt"
+fhand=open(filename)
+lst=list()
+sec=dict()
+
+for line in fhand:
+  if line.startswith('From:'):
+    continue
+  if line.startswith('From') :
+    words=line.split()
+    words=words[1]
+    sec[words]=sec.get(words,0)+1
+print(words,sec[words]) 
+"""for line in fhand:
+  if line.startswith("From:") or not line.startswith('From'):
+    continue
+    word=line.split
+    lst.append(word[1])
+print(lst)
+for word in sec.items():"""
+  
+   
+
+  
+    
