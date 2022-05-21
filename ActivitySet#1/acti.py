668a452c9afd77f610403d25c69da00e6a2c8cf5
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-05-21 15:27:44 +0000

    Q.1

---
+++ ActivitySet#1/acti.py
@@ -0,0 +1,13 @@
+x=input #global varible
+x("hi")
+print(x)
+input=20  #local varible
+input("hi")
+"""
+Here x is a global variable , and input is local variable inside  global function 
+In this we understood that global variable can use  any where in the in code ...not local variable(local variable has scope only inside the local function)
+
+
+x=input 
+without this line code doesn't work
+"""
