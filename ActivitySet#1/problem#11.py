418f7d812a62faf2f4fbc39aba07c581b8f82a54
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-05-21 16:22:51 +0000

    P11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -5,3 +5,28 @@ From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
 """
 filename = "dataset/mbox-short.txt"
+lst=list()
+d=dict()
+if len(filename) < 1:
+    filename = "mbox-short.txt"
+handle = open(filename)
+for line in handle:
+    if line.startswith("From:"):
+         continue
+    if line.startswith("From"):
+         l=line.split()
+         t=l[5]
+         
+         ft=t.split(":")
+         lst.append(ft[0])
+         lst.sort() 
+"""for c in lst:
+    d[c]=d.get(c,0)+1
+    print(c,d[c])"""
+         
+for k in lst:
+    if k not in d:
+      d[k]=1
+    else:
+        d[k]+=1
+print(d)
