c08e94aa01cf5659dc9f6f510a80cd735ae421d8
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-05-22 15:45:52 +0000

    romeo

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,10 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+filename=input("Enter the file name: ")
+fopen=open(filename)
+for line in fopen:
+  line=line.rstrip()
+  if re.search('^F..m',line):
+  
+   print(line)
