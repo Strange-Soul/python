4663a0be661524205a4788c0b51339e39f727a1f
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-05-30 16:43:34 +0000

    p12..

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -2,10 +2,14 @@
 #https://www.py4e.com/lessons/regex
 
 import re
+sum=0
 file_name=input("Enter the filename:")
-file_open=open(file_name,"r+")
+file_open=open(file_name,"r")
 for line in file_open:
   line =line.rstrip()
-  if re.search("",line):
-    print(line)
+  find_num=re.findall("[0-9]+",line)
+  for num in find_num:
+    y=float(num)
+    sum=sum+y
+print(sum)
   
