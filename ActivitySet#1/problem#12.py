67279f4398216389cd5690dc41b83555d142cb6e
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-05-31 10:32:47 +0000

    P12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -7,9 +7,9 @@ file_name=input("Enter the filename:")
 file_open=open(file_name,"r")
 for line in file_open:
   line =line.rstrip()
-  find_num=re.findall("[0-9]+",line)
+  #find_num=re.findall("[0-9]+",line)
+  find_num=re.findall('\d+',line)
   for num in find_num:
     y=float(num)
     sum=sum+y
 print(sum)
-  
