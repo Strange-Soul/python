25f62e3251a9ef8361f403dda352271b581db566
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-06-01 17:24:23 +0000

    P10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -3,16 +3,38 @@
 """
 9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
 """        
-filename=input("Enter the file_name:")
-fhandle=open(filename) 
-word_count=dict() #dct={}
+"""filename=input("Enter the file name to open :")
+fopen=open(filename)
+dt=dict()
+for line in fopen:
+  print(line)"""
+file_name=input("Enter the FileName to open:")
+fhandle=open(file_name)
+lst=list()
+d=dict()
 for line in fhandle:
   if line.startswith("From:"):
-    line=line.rstrip()
-    e_mails=line.split()
-    #print(e_mails[1])
-    repeat_word=e_mails[1]
-    word_count[repeat_word]=word_count.get(repeat_word,0)+1
-for key,value in word_count.items():
-    print("E-mail:",key,'....',"Number_of_times_appered:",value)
-print(repeat_word,word_count[repeat_word])
+   continue
+  if line.startswith("From"):
+    spt=line.split()
+    sp=spt[1]
+    d[sp]=d.get(sp,0)+1
+    #d[spt[1]]=d.get(spt[1],0)+1
+print(sp,d[sp])
+#print(spt[1],d[spt[1]])
+   
+"""file_name=input('Enter the file name to open:')
+fhandle=open(file_name)
+lst=list()
+dt=dict()
+for line in fhandle:
+  if line.startswith("From:"):
+   continue
+  if line.startswith('From'):
+   spt=line.split()
+   lst.append(spt[1])
+for key in lst:
+  dt[key]=dt.get(key,0)+1
+print(dt)
+for key,value in dt.items():
+ print(key,value)"""
