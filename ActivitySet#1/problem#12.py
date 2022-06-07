a318f53036168bf95a4c74389b4f6a36fb2b1258
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-06-07 10:52:23 +0000

    P12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -3,12 +3,11 @@
 
 import re
 sum=0
-file_name=input("Enter the filename to open:")
+file_name=input("Enter the file name to open:")
 file_open=open(file_name)
 for line in file_open:
   line=line.strip()
-  #find_name=re.findall('\d+',line)
-  find_num=re.findall("[0-9]+",line)
+  find_num=re.findall('[0-9]+',line)
   for num in find_num:
     y=float(num)
     sum+=y
