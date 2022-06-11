1f58297b17176fa1a55da304bdd2c88d3eb552a5
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-06-11 07:00:05 +0000

    P5

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -10,7 +10,7 @@ Score Grade
 If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
 """
 
-marks=input("Enter the Score > ")
+"""marks=input("Enter the Score > ")
 score=float(marks)
 if (score<0.0) or (score>1.0):
     print("Out of Range ")
@@ -28,4 +28,26 @@ elif (score >= 0.0) and (score <= 1.0):
     elif (score < 0.6):
       print("F")
 else:
- print("All are done")
+ print("All are done")"""
+mark=input('Enter the score:')
+try:
+  marks=float(mark)
+except:
+  print("Invalid character \n Re_enter the digit !!!")
+  
+if marks >1.0 or marks <0.0:
+  print("Out of range!!")
+elif marks>0.0 and marks<1.0:
+  print("Calculating the Grade..... \n please wait for a moment!!!")
+  if marks>=0.9:
+    print("Grade:'A'")
+  elif marks>=0.8:
+    print("Grade:'B'")
+  elif marks>=0.7:
+    print("Grade:'C'")
+  elif marks>=0.6:
+    print("Grade:'D'")
+  elif marks<0.6:
+    print("Grade:'F'")
+else:
+  print("All Done !!!")
