8e49f17d690dbf0e97f033265c3fce04b2c40249
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-06-13 15:52:52 +0000

    P13

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,4 +1,21 @@
 # Network Programming
 #https://www.py4e.com/lessons/network
 
-impi
+from socket import *
+soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+import socket
+T_PORT=60
+TCP_IP='127.000.000.011'
+BUF_SIZE=30
+soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+soc.bind((TCP_IP,T_PORT))
+soc.listen(1)
+con,addr=soc.accept()
+print("Connection Address is :",addr)
+while True:
+    data=con.recv(BUF_SIZE)
+    if not data:
+     break
+    print("Recived data",data)
+con.send(data)
+
