4cd3ef38eb0759c89d315aeb0319e0edf95069e2
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-06-14 10:09:54 +0000

    P09

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -3,8 +3,8 @@
 Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in alphabetical order.
 You can download the sample data at http://www.py4e.com/code3/romeo.txt
 """
-#filename = "dataset/romeo.txt"
-filename=input("Enter the file_name:")
+filename = "dataset/romeo.txt"
+#filename=input("Enter the file_name:")
 fopen=open(filename)
 lst=list()
 for line in fopen:
