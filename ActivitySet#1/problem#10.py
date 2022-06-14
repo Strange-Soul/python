4bda54e2a1d5a3261f963a9c931e4a72a4d92400
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-06-14 10:28:54 +0000

    P10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -3,7 +3,7 @@
 """
 9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
 """        
-file_name="dataset/mbox-short.txt"
+'''file_name="dataset/mbox-short.txt"
 fhandle=open(file_name)
 d=dict()
 for line in fhandle:
@@ -14,20 +14,14 @@ for line in fhandle:
     email=spt[1]
     d[email]=d.get(email,0)+1
 print(email,d[email])
-#print(spt[1],d[spt[1]])
-   
-"""file_name=input('Enter the file name to open:')
-fhandle=open(file_name)
-lst=list()
-dt=dict()
-for line in fhandle:
-  if line.startswith("From:"):
-   continue
-  if line.startswith('From'):
-   spt=line.split()
-   lst.append(spt[1])
-for key in lst:
-  dt[key]=dt.get(key,0)+1
-print(dt)
-for key,value in dt.items():
- print(key,value)"""
+#print(spt[1],d[spt[1]])'''
+
+email_s=dict()
+file='dataset/mbox-short.txt'
+file_open=open(file)
+for line in file_open:
+  if line.startswith('From '):
+    line=line.rstrip().split()
+    email=line[1]
+    email_s[email]=email_s.get(email,0)+1
+print(email,email_s[email])
