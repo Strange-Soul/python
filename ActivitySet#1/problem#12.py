1fb60ef07d9ccf9a25fa37097fb1cdeb00665a7d
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-06-21 14:10:49 +0000

    P12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,7 +1,7 @@
 # Regular Expressions
 #https://www.py4e.com/lessons/regex
 
-import re
+'''import re
 sum=0
 file_name=input("Enter the file name to open:")
 file_open=open(file_name)
@@ -12,6 +12,17 @@ for line in file_open:
   for num in find_num:
     y=float(num)
     sum+=y
-print(sum)
+print(sum)'''
 
+import re
+sum=0
+file_name=input('Enter the file_name:')
+file_open=open(file_name)
+for line in file_open:
+  line=line.rstrip()
+  find_num=re.findall('[0-9]+',line)
+  for num in find_num:
+    y=float(num)
+    sum+=y
+print("Sum of digits is :",sum)
  
