eb61bd0f5e90b845b1d09383bfcfefcef0477a09
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-06-23 15:43:26 +0000

    P 13

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,7 +1,20 @@
 # Network Programming
 #https://www.py4e.com/lessons/network
+import socket
+host='data.pr4e.org'
+port=80
+address=(host,port)
+my_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+my_sock.connect(address)
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 
-from socket import *
+while True:
+    data=my_sock.recv(1024)
+    if len(data)<1:
+      break
+    print(data.decode(data),end="")
+my_sock.close()
+'''from socket import *
 soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 import socket
 T_PORT=60
@@ -18,4 +31,4 @@ while True:
      break
     print("Recived data",data)
 con.send(data)
-
+'''
