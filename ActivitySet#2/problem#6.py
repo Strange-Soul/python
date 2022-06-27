2061085e75ce46a469979efd81e7441b1262271c
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-06-27 16:11:11 +0000

    P6

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,11 +1,20 @@
+print("\033[1m Menu List \033[0m")
+class Menu():
+  """fill in class definition here"""
+  def __init__(self,item,rate):
+    self.item=item
+    self.rate=rate
 
+  def show(self):
+    print(f" Item is :\033[1m '{self.item}' \033[0m,rate = \033[1m '{ self.rate}' \033[0m ")
 
-class Menu:
-    """fill in class definition here"""
+idly=Menu("Idly",'20')
+vada=Menu("Vada",'10')
+dosa=Menu("Dosa",'25')
+poori=Menu("Poori",'30')
 
+poori.show()
+idly.show()
+vada.show()
+dosa.show()
 
-m = Menu()  # Menu is a class
-m.add("idly", 10)
-m.add("vada". 20)
-
-m.show()
