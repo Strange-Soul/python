f600923656290011e3dff485d330401bfa927349
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-07-01 04:52:07 +0000

    p20

--- ActivitySet#1/problem#20.py
+++ ActivitySet#1/problem#20.py
@@ -1,4 +1,26 @@
-import re
+ #Networking Program (Web_Scrapin)
+from urllib.request import Request,urlopen
 from bs4 import BeautifulSoup
-from urllib.request import urlope
-n
+
+url=input('\033[1m Enter the Link \033[0m ->')
+#count=int(input("Enter the count(Repeatation): "))
+#pos=int(input("Enter the position :"))
+
+html_content=urlopen(url)
+#print(html_content)
+
+soup=BeautifulSoup(html_content,"lxml")
+all_links=[]
+for link  in soup.find_all('a'):
+    #print(link.get('href'))
+    all_links.append(link.get('href'))
+for i in range(7):
+    url = Request(all_links[17])
+    html_content = urlopen(url)
+    soup = BeautifulSoup(html_content,"lxml")
+    for title in soup.find_all("title"):
+        print(title.get_text())
+    all_links.clear() 
+    for i in soup.find_all('a'):
+        all_links.append(i.get('href'))
+    
