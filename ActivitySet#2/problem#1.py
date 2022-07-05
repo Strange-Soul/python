12abe071b2dbe6b6e1400288a829a4a9ccffdd5a
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-07-05 15:57:05 +0000

    P1

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -4,9 +4,11 @@ def add(a, b):
 
 
 def main():
-    a = int(input("Enter 1st Number: "))
-    b = int(input("Enter 2nd Number: "))
+    a = int(input("Enter 1st Number? "))
+    b = int(input("Enter 2nd Number? "))
 
     c = add(a, b)
-    print("sum of two numbers is : ",c)
-main()
+    print(f"sum of {a} and {b} is  {c}")
+
+if __name__=='__main__':
+ main()
