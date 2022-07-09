023586a49843e91c155ba40ae54dd0d22710d700
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-07-09 14:38:59 +0000

    P5

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -6,23 +6,38 @@ def get_str():
 
 def str_to_dict(str):
   """convert connect string to a dictionary"""
-  dict={}
+  l=[]
+  t=tuple()
   str=str.split(';')
   for i in str:
-    sub=i.split('=')
-    for x in sub:
-      
-    
-  
+    t=tuple(i.split('='))
+    l.append(t)
+    dict={wrd[0]:wrd[1] for wrd in l}
+    ''' for wrd in l:
+    dict={wrd[0]:wrd[1]}
+    l.append((dict.items))
+    print(list)'''
+  #print(dict)
+  return dict
 
 def dict_to_str(d):
   """convert a dictionary to connect string"""
   str=''
-  for i in d.items():
-    str+=
+  for k,v in d.items():
+    str+=k+'='+v+';'
+  return str
   
 def main():
-   
+   n=get_str()
+   dictionary=str_to_dict(n)
+   print(dictionary)
+   string=dict_to_str(dictionary)
+   print(string)
   
+
 if __name__ == '__main__':
     main()
+
+'''s="a=b;c=d;e=f;g=h"
+>>> {(a:=i.split("="))[0]:a[1] for i in s.split(";")}
+{'a': 'b', 'c': 'd', 'e': 'f', 'g': 'h'}'''
