86ea994db949b6b816ee67d74a046c29d9e9f8d2
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-07-09 15:44:03 +0000

    P7

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,10 +1,17 @@
 
+class Menu():
+  """fill in class definition here"""
+  def __init__(self,item,rate):
+    self.item=item
+    self.rate=rate
+  def __add__(self,other):
+    return self.item + str(self.rate),other.item+str(other.rate)
 
-class Menu:
-    """fill in class definition here"""
 
 
-m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
-
-print(m)  # should print the menu properly
+m1=Menu('idly',10)
+m2=Menu('vada', 20)
+m3=Menu('dosa',30)
+# Hint: operator overloading special methods (__add__, __sub__, etc.)
+print("__________Menu___________")  # should print the menu properly
+print(m1 +m2)
