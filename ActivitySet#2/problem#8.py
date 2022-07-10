33aca34c1e28a0900e3ad570eeec0b487528fe34
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-07-10 07:21:24 +0000

    P8

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -2,10 +2,15 @@ class Menu:
   """fill in class definition here"""
   def __init__(self,item,rate):
     self.item=item
-    self.rate=rate
+    self.rate=int(rate)
+  def __setattr__(self,item,rate):
+    print(f"You set {item} ={rate} ")
+    self.__dict__[item]=rate
+  
+m = Menu('Idly',56)
+print(f"Item = {m.item} rate={m.rate}")
+m.vada=20
+m.dosa=30
+print("\033[1m Printing Menu in Dictionary Type \033[0m")
+print(m.__dict__)
 
-m = Menu()
-m["idly"] = 10
-m["vada"] = 20
-
-print(m)
