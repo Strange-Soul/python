e2116135e00cec7db640c3323e487d85f71b0b6d
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-07-17 15:02:38 +0000

    P9

--- ActivitySet#2/problem#9.py
+++ ActivitySet#2/problem#9.py
@@ -1,45 +1,60 @@
-'''class Menu(dict):
+import os
+os.system('clear')
+class Menu(dict):
   """fill in class definition here"""
-  def __init__(item,rate):
-    self.item=item
-    self.rate=rate
+  def __init__(self):
+    pass
+  def dict(self,item,rate):
+    self[item]=rate
 
-class Order:
+class Order(Menu):
   """fill in class definition here"""
+  
+  def __init__(self,item,rate):
+    Menu.__init__(self,item,rate)
+  def display(self):
+    if input=='y':
+      print("Which item do u want ??")
+      print()
+
   
 
 class Bill:
   """fill in class definition here"""
-
+  
 
 m = Menu()
-m["idly"] = 20
-m["vada"] = 20
+m['Pongal']=30
+m["Idly"] = 10
+m["Vada"] = 20
+m['Dosa']=25
+m['Lemon Rice']=30
+print('Hi welcome to Hotel')
+choice=input("Do u like to see the Menu y/n:")
+if choice =='y':
+ print("We have the following Items")
+ print('Menu'.center(20,'-'))
+ i=0
+ for k,v in m.items():
+   i+=1
+   print(str(i)+'.'+k,'->',v)
+ sel=int(input("Enter the index of our favourite item from above Menu : "))
+ for i in m.items():
+    if sel==i:
+     print(f'Your selection is {m.key()}')
+    
+else:
+  exit()
 
-o = Order(m)
+
+'''o = Order(m)
 try:
     o["vada"] = 2
     o["pongal"] = 2
 
 except KeyError as e:
     print(e)
-
-b = Bill(m, o)
+'''
+'''b = Bill(m, o)
 print(b)
 '''
-class Menu(dict):
-  def __init__(self):
-    self=dict()
-
-  def add(self,item,price):
-    self[item]=price
-
-  def show(self):
-    for i,j in self.items():
-        print(i,j)
-
-
-m = Menu()
-m.add('idly', 10)
-m.add('vada', 20)
-m.show()
