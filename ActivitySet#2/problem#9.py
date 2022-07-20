0ff5b791b8af3f7944962c3b3f22d3c56a13cf17
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-07-20 15:17:56 +0000

    P9

--- ActivitySet#2/problem#9.py
+++ ActivitySet#2/problem#9.py
@@ -5,55 +5,51 @@ class Menu(dict):
   def dict(self,item,rate):
     self[item]=rate
 
-class Order(Menu):
+class Order():
   """fill in class definition here"""
-  
-  def __init__(self,item,qty):
-   Menu.__init__(item)
-   
-   self.qty=0
-   
-  def dict(self,item,qty):
-    self[item]=qty
+  def __init__(self,menu):
+   self.menu=menu
+   self.orders=dict()
+  def __setitem__(self,item,qty):
+    self.orders[item]=qty
+    
 
-class Bill(Order):
+class Bill:
   """fill in class definition here"""
-  def __init__(self,item,rate,qty):
-    #Menu.__init(self,item,rate)
-    Order.__init__(self,item,qty)
-  def selection(self):
-    print(f"U selected {self.qty}->{self.item}")
+  def __init__(self,item,order):
+    self.bill=sum(v*self.item[k] for k,v in order.orders.items())
+  def __str__(self):
+    return f"{self.bill} rupees"
+
 m = Menu()
-m['Pongal']=30
-m["Idly"] = 10
-m["Vada"] = 20
-m['Dosa']=25
-m['Lemon Rice']=30
+m['pongal']=30
+m["idly"] = 10
+m["vada"] = 20
+m['dosa']=25
+m['lemon Rice']=30
 m['Tea']=8
 print('Hi welcome to Hotel')
 choice=input("Do u like to see the Menu y/n:")
 if choice =='y':
- print("We have the following Items")
- print('Menu'.center(20,'-'))
- i=0
- for k,v in m.items():
+  print("We have the following Items")
+  print('Menu'.center(20,'-'))
+  i=0
+  for k,v in m.items():
    i+=1
    print(str(i)+'.'+k,'->',v)
-    
 else:
-  exit()
+   exit()
+print("Accepting your Order".center(50,'>'))
 
-print("Accepting your Order")
-o = Order()
+o = Order(m)
 try:
     o["vada"] = 2
     o["pongal"] = 2
-  
 except KeyError as e:
     print(e)
-print(f"U selected {o.qty}->{o.item} ")
+for k,v in o.item():
+ print(f"U selected {v}->{k} ")
 
-b = Bill(m, o)
-b=o.qty*m.rate
-print(b)
+
+
 
