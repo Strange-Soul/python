ee4f6a28463236a2e9a79576a0d489e0d8d7c23e
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-07-23 13:45:05 +0530

    Update problem#1.py

--- ActivitySet#3/problem#1.py
+++ ActivitySet#3/problem#1.py
@@ -1,5 +1,48 @@
-class rectangle:
-    def __init__(self):
-      self.x1,self.y1,self.x2,self.y2,self.x3,self.y3=map(float,input("Enter the points:")).split()
+class point:
+  
+  def __init__(self):
+    self.x=[]
+    self.y=[]
+  def __setitem__(self):
+    n=int(input("Enter the Number of co-ordinates: "))
+   
+    for i in range(1,n+1):
+       x=float(input(f"Enter the {i}'x'co-ordinate:"))
+       y=float(input(f"Enter the {i} 'y' co-ordinate:"))
+       self.x.append(x)
+       self.y.append(y)
+       i+=1
+    #print(self.x,self.y)
+class Rectangle(point):
+  def __init__(self):
+    super().__init__
+    self.dist=0
+  def cal_dist(self):
+    
+    self.dist1=math.sqrt((pt.x[1]-pt.x[0])**2-(pt.y[1]-pt.y[0])**2)
+    self.dist2=math.sqt((pt.x[2]-pt.x[1])**2-(pt.y[2]-pt.y[1])**2)
+    self.dist3=math.sqrt((pt.x[2]-pt.x[0])**2-(pt.y[2]-pt.y[0])**2)
+    print('Dis-1:',self.dist1,'Dist-2:',self.dist2,'Dist3:',self.dist3)
+  def __gt__(self):
+    if self.dist1> self.dist2  & self.dist1>self.dist3:
+      self.dist=self.dist2*self.dist3
+    elif self.dist2> self.dist1 & self.dist2>self.dist3:
+      self.dist=self.dist1*self.dist3
+    else:
+      self.dist=self.dist1*self.dist2
+   
+    #return (self.dist2*self.dist3 if self.dist1> self.dist2 and self.dist1>self.dist3  else self.dist1*self.dist3  )
+      
+x,y=[],[]
+pt=point()
+pt.__setitem__()
+print("X co-ordinate:",pt.x)
+print("Y co-ordinates:",pt.y)
+x_y=[(x,y) for x,y in zip(pt.x,pt.y)]
+print(tuple(x_y))
+#x_y=[i+j for i,j in (pt.)]
+rt=Rectangle()
+print(rt.cal_dist())
 
-points=rectangle()
+
+
